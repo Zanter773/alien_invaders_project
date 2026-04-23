9b6171376262593b487586eb2a5117576b8541ea
Nathan Brooks <nathangamer0806@gmail.com>
2026-04-23 13:49:35 -0400

    Revert "Lives now visible / Game completed"
    This reverts commit 0cf462d5c39eaaf3bcb9bfdfe127d03a1844bdb0.

--- alien_invasion.py
+++ alien_invasion.py
@@ -87,7 +87,6 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.sb.prep_score()
             self.sb.prep_level()
-            self.sb.prep_ships()
 
             self.game_active = True
 
@@ -234,9 +233,8 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
         if self.stats.ships_left > 0:
-            # Decrement ships_left, and update scoreboard.
+            # Decrement ships_left.
             self.stats.ships_left -= 1
-            self.sb.prep_ships()
 
             # Get rid of any remaining bullets and aliens.
             self.bullets.empty()
