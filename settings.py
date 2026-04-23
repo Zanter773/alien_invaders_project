5c651214e61bad22e003b564c57d74f3cc918928
Nathan Brooks <nathangamer0806@gmail.com>
2026-04-23 16:30:55 -0400

    Messing Around

--- settings.py
+++ settings.py
@@ -18,11 +18,10 @@ class Settings:
         self.ship_limit = 3
 
         # Bullet settings
-        self.bullet_speed = 3.0
         self.bullet_width = 15
         self.bullet_height = 3
         self.bullet_color = (60, 60, 60)
-        self.bullet_allowed = 3
+        self.bullet_allowed = 6
 
         # Alien Settings
         self.fleet_drop_speed = 20
@@ -42,7 +41,6 @@ class Settings:
         self.ship_speed = 1.5
         self.bullet_speed = 4.0
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 50
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
 
